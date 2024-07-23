f1d4c6dbe3851c05bf3d4be29717f732ffe69b0e
xiaochun <18042542@qq.com>
2024-07-23 11:17:40 +0800

    ...

--- zaplog/zaplog.go
+++ zaplog/zaplog.go
@@ -8,33 +8,33 @@ import (
 	"github.com/natefinch/lumberjack"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"os"
 	"time"
 )
 
-func NewZapLog(serviceId, serviceName, serviceVersion string) log.Logger {
+func NewZapLog(serviceId, serviceName, serviceVersion string, maxBackups, maxAge int, mode string) log.Logger {
 	// 分割日志文件
 	lumberjackLogger := &lumberjack.Logger{
 		Filename:   fmt.Sprintf("./logs/%s.log", serviceName),
 		MaxSize:    500, // megabytes
-		MaxBackups: 10,
-		MaxAge:     7,     //days
-		Compress:   false, // 禁用压缩
-		LocalTime:  true,  // 使用本地时间作为文件名的时间戳
+		MaxBackups: maxBackups,
+		MaxAge:     maxAge, //days
+		Compress:   false,  // 禁用压缩
+		LocalTime:  true,   // 使用本地时间作为文件名的时间戳
 	}
 	defer lumberjackLogger.Close()
 
 	zapConfig := zap.NewProductionEncoderConfig()
 	zapConfig.EncodeTime = zapcore.TimeEncoderOfLayout(time.DateTime)
 
-	zapCores := make([]zapcore.Core, 0)
+	// console
+	core := zapcore.NewCore(zapcore.NewConsoleEncoder(zapConfig), zapcore.Lock(os.Stdout), zapcore.DebugLevel)
+	if mode == "release" {
+		// 输出到文件
+		zapcore.NewCore(zapcore.NewConsoleEncoder(zapConfig), zapcore.AddSync(lumberjackLogger), zap.InfoLevel)
+	}
 
-	// 文件
-	//zapCores = append(zapCores, zapcore.NewCore(zapcore.NewJSONEncoder(zapConfig), zapcore.AddSync(lumberjackLogger), zap.InfoLevel))
-	zapCores = append(zapCores, zapcore.NewCore(zapcore.NewConsoleEncoder(zapConfig), zapcore.AddSync(lumberjackLogger), zap.InfoLevel))
-
-	//zapcore.NewCore(zapcore.NewConsoleEncoder(zapConfig), zapcore.Lock(os.Stdout), zapcore.DebugLevel),   // console
-
-	zapLogger := zap.New(zapcore.NewTee(zapCores...))
+	zapLogger := zap.New(zapcore.NewTee(core))
 	defer zapLogger.Sync()
 
 	return log.With(zapLog.NewLogger(zapLogger),
