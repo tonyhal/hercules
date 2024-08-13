0d220d6162a0f8721c72d46c3edffb1b9b75360e
xiaochun <18042542@qq.com>
2024-08-13 20:07:54 +0800

    ...

--- gormlog/gromlog.go
+++ gormlog/gromlog.go
@@ -27,6 +27,7 @@ type GormLogger struct {
 func NewGormLogger() *GormLogger {
 	return &GormLogger{
 		SlowThreshold: 900 * time.Millisecond, // 一般超过200毫秒就算慢查所以不使用配置进行更改
+		LogLevel:      logger.Info,
 	}
 }
 
@@ -40,15 +41,21 @@ func (l *GormLogger) LogMode(level logger.LogLevel) logger.Interface {
 }
 
 func (l *GormLogger) Info(ctx context.Context, msg string, data ...interface{}) {
-	log.Context(ctx).Infof(msg, data)
+	if l.LogLevel >= logger.Info {
+		log.Context(ctx).Infof(msg, data)
+	}
 }
 
 func (l *GormLogger) Warn(ctx context.Context, msg string, data ...interface{}) {
-	log.Context(ctx).Errorf(msg, data)
+	if l.LogLevel >= logger.Warn {
+		log.Context(ctx).Warnf(msg, data)
+	}
 }
 
 func (l *GormLogger) Error(ctx context.Context, msg string, data ...interface{}) {
-	log.Context(ctx).Errorf(msg, data)
+	if l.LogLevel >= logger.Error {
+		log.Context(ctx).Errorf(msg, data)
+	}
 }
 
 func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
@@ -57,16 +64,12 @@ func (l *GormLogger) Trace(ctx context.Context, begin time.Time, fc func() (sql 
 	// 获取 SQL 语句和返回条数
 	sql, rows := fc()
 
-	// Gorm 错误时打印
-	if err != nil && !errors.Is(err, gorm.ErrRecordNotFound) {
+	switch {
+	case err != nil && l.LogLevel >= logger.Error && !errors.Is(err, gorm.ErrRecordNotFound): // 错误时打印
 		log.Context(ctx).Errorf("SQL ERROR, | err=%v elapsed=%v, rows=%v, sql=%v ", err.Error(), elapsed, rows, sql)
-	}
-	// 慢查询日志
-	if l.SlowThreshold != 0 && elapsed > l.SlowThreshold {
-		log.Context(ctx).Warnf("Database Slow Log, | elapsed=%v, rows=%v, sql=%v", elapsed, rows, sql)
-	}
-
-	if l.LogLevel == logger.Info {
+	case l.SlowThreshold != 0 && elapsed > l.SlowThreshold: // 慢查询日志
+		log.Context(ctx).Warnf("Slow SQL, | elapsed=%v, rows=%v, sql=%v", elapsed, rows, sql)
+	case l.LogLevel == logger.Info: // 日志详情
 		log.Context(ctx).Infof("SQL Info, | elapsed=%v, rows=%v, sql=%v", elapsed, rows, sql)
 	}
 }
