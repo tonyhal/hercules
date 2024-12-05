a386dccbaf46f71e5546e36d51b475503e1f1c4a
xiaochun <18042542@q.com>
2024-12-05 21:46:40 +0800

    ...

--- binlog/master.go
+++ binlog/master.go
@@ -50,7 +50,7 @@ func (m *master) Save(gtidSet string) error {
 		return nil
 	}
 	n := time.Now()
-	if n.Sub(m.lastSaveTime) < time.Minute {
+	if n.Sub(m.lastSaveTime) < 30*time.Second {
 		return nil
 	}
 
